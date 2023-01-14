5e954118894699f1df49500212d2627b295cba36
Riya Verma <48110227+Riya2010@users.noreply.github.com>
2023-01-13 16:42:36 -0800

    Create submit.sh

---
+++ submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export REPO_PATH=/project/rsubrama_477/riya/VariantCalling
+
+if [ ! -e "slurm_logs" ]
+then
+  mkdir "slurm_logs"
+fi
+
+snakemake \
+--use-conda \
+--debug-dag \
+--snakefile $REPO_PATH/Snakefile \
+--configfile $REPO_PATH/config.yaml \
+--printshellcmds \
+--keep-going \
+--rerun-incomplete \
+--cluster-config $REPO_PATH/cluster.yaml \
+--cores 50 \
+--cluster 'sbatch --ntasks={cluster.tasks} --cpus-per-task={cluster.cores} --mem={cluster.mem} --time={cluster.time} --account={cluster.acc} --output {cluster.logout} --error {cluster.logerror}' \
+$@
